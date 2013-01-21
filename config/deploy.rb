b9f4612863d3ba3269225e47c06ebbaeb77e6064
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-01-22 02:58:18 +0700

    deploy and symlink mongoid

--- config/deploy.rb
+++ config/deploy.rb
@@ -122,6 +122,7 @@ namespace :deploy do
 
   task :symlink_database_and_system_folder do
     run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/mongoid.yml #{current_path}/config/mongoid.yml"
   end
 
   task :invoke_rake do
