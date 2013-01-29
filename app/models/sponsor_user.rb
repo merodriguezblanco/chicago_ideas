fca48b07f36deb3ff2b0d9e03501f2df265f5e93
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-01-29 13:44:22 +0700

    when set SponsorUser as primary contact. remove other primary contact status

--- app/models/sponsor_user.rb
+++ app/models/sponsor_user.rb
@@ -1,4 +1,14 @@
 class SponsorUser < ActiveRecord::Base
   belongs_to :sponsor
   belongs_to :user
+  
+  before_save :check_primary
+  
+  private
+    def check_primary
+      if self.primary_contact == true
+        SponsorUser.where(sponsor_id: self.sponsor_id, primary_contact: true).update_all(primary_contact: false)
+        self.primary_contact = true
+      end
+    end
 end
