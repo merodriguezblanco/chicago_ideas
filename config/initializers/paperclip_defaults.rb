cd5965c48ff042af9eeebad3d83dad6e09818bf4
Duc Le <duc@meetmantra.com>
2013-01-30 21:30:38 +0700

    fixed path for Paperclip attachments on S3

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -7,6 +7,6 @@ else
   Paperclip::Attachment.default_options.update({
     :storage        => :s3,
     :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-    :path           => ":class/:attachment/:id/:style/:filename"
+    :path           => ":class-:attachment/:style/:id.:extension"
   })
 end
