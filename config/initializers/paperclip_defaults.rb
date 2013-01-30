6f286987d85097a7ad04cd9d8a4f1768b179e7c7
Duc Le <duc@meetmantra.com>
2013-01-30 22:26:58 +0700

    use https protocol for S3

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -7,6 +7,7 @@ else
   Paperclip::Attachment.default_options.update({
     :storage        => :s3,
     :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-    :path           => ":class-:attachment/:style/:id.:extension"
+    :path           => ":class-:attachment/:style/:id.:extension",
+    :s3_protocol => 'https'
   })
 end
