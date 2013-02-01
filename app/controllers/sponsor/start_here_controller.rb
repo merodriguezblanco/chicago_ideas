e3f9e054723eb4abc4e95f88d5668dbfdfb35641
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-02-01 13:06:51 +0700

    automatic unlocking sponsor

--- app/controllers/sponsor/start_here_controller.rb
+++ app/controllers/sponsor/start_here_controller.rb
@@ -11,8 +11,13 @@ class Sponsor::StartHereController < Sponsor::BaseController
   
   def update_sponsor
     @sponsor = current_user.sponsor
+    do_unlock = false
+    if @sponsor.logo_file_name.blank? || @sponsor.eps_logo_file_name.blank? || @sponsor.sponsor_agreement_file_name.blank?
+      do_unlock = true if @sponsor.locked?
+    end
     if @sponsor.update_attributes(params[:sponsor])
       update_primary_contact
+      unlock if do_unlock
       redirect_to sponsor_root_path, notice: "Sponsor was saved successfully"
     else
       @errors = @sponsor.errors.full_messages
@@ -28,4 +33,10 @@ class Sponsor::StartHereController < Sponsor::BaseController
       end
     end
   
+    def unlock
+      unless (@sponsor.logo_file_name.blank? || @sponsor.eps_logo_file_name.blank? || @sponsor.sponsor_agreement_file_name.blank?)
+        @sponsor.locked = false
+        @sponsor.save
+      end
+    end
 end
