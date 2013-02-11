95afd7a43105eff9330f57cbc7f95fe17777ed0c
Leandro Marcucci <leanucci@gmail.com>
2013-02-11 19:45:56 -0300

    Order processing methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
+require 'authorize_net'
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :member_type
@@ -20,5 +21,19 @@ class Order < ActiveRecord::Base
   validates :expiry_month, :presence => true, :length => { :is => 2 }
   validates :cvc, :presence => true, :length => { :is => 3 }
 
+  def process_transaction
+    transaction = AuthorizeNet::AIM::Transaction.new(AUTHNET_LOGIN, AUTHNET_KEY, :gateway => AUTHNET_ENV)
+    credit_card = AuthorizeNet::CreditCard.new(card_number, expiry_date)
+    response = transaction.purchase(member_type.price, credit_card)
 
+    return response.success?
+  end
+
+  def expiry_date
+    "#{expiry_month}#{expiry_year}"
+  end
+
+  def create_member
+    user = User.find_or_create_by_email(self.email, :password => self.password)
+  end
 end
