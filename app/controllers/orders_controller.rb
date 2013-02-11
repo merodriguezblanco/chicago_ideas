9cbba8d2a5b9579d297f278ad48477571d7731dc
Leandro Marcucci <leanucci@gmail.com>
2013-02-11 19:46:20 -0300

    Basic checkout flow

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,18 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
-    @order.member_type = MemberType.find(params[:member_id])
+    @member_type = MemberType.find(params[:member_id])
+  end
+
+  def create
+    @order = Order.new(params[:order])
+    @order.member_type = MemberType.find(params[:order][:member_type_id])
+
+    if @order.process_transaction && @order.save
+      redirect_to '/', :notice => "member!"
+    else
+      render :new
+    end
   end
 
 end
