17430877aa3686cea65173b81b55ae07468e55dd
Leandro Marcucci <leanucci@gmail.com>
2013-02-11 14:16:24 -0300

    New order form

---
+++ db/migrate/20130208190228_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.integer :total_in_cents
+      t.boolean :approved
+      t.integer :user_id
+      t.integer :member_type_id
+
+      t.timestamps
+    end
+  end
+end
