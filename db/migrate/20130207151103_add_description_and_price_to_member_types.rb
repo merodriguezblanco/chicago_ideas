f75c825a52d452fc1d0bca5a88a72847f9532a9c
Leandro Marcucci <leanucci@gmail.com>
2013-02-12 12:10:57 -0300

    Default 0 for member_types.price_in_cents

--- db/migrate/20130207151103_add_description_and_price_to_member_types.rb
+++ db/migrate/20130207151103_add_description_and_price_to_member_types.rb
@@ -1,6 +1,6 @@
 class AddDescriptionAndPriceToMemberTypes < ActiveRecord::Migration
   def change
     add_column :member_types, :description, :text
-    add_column :member_types, :price_in_cents, :integer
+    add_column :member_types, :price_in_cents, :integer, :default => 0
   end
 end
