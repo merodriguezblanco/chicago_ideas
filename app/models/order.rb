8e824f8a133e4278a84f187e5678940030334e25
Leandro Marcucci <leanucci@gmail.com>
2013-02-12 16:07:06 -0300

    Better routing for checkout & form errors

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ActiveRecord::Base
   belongs_to :member_type
 
   attr_accessor :name_on_card, :email, :address, :city, :state, :card_number,
-                :expiry_month, :expiry_year, :cvc, :zip, :password
+                :expiry_date, :cvc, :zip, :password
 
 
   validates_presence_of :name_on_card
@@ -14,23 +14,20 @@ class Order < ActiveRecord::Base
   validates_presence_of :state
   validates_presence_of :card_number
   validates_presence_of :zip
-  validates_presence_of :username
   validates_presence_of :password
 
-  validates :expiry_year, :presence => true, :length => { :is => 2 }
-  validates :expiry_month, :presence => true, :length => { :is => 2 }
+  validates :expiry_date, :presence => true, :length => { :is => 4 }
   validates :cvc, :presence => true, :length => { :is => 3 }
 
   def process_transaction
-    transaction = AuthorizeNet::AIM::Transaction.new(AUTHNET_LOGIN, AUTHNET_KEY, :gateway => AUTHNET_ENV)
+    transaction = AuthorizeNet::AIM::Transaction.new(AUTHNET_LOGIN,
+                                                     AUTHNET_KEY,
+                                                     :gateway => AUTHNET_ENV)
     credit_card = AuthorizeNet::CreditCard.new(card_number, expiry_date)
     response = transaction.purchase(member_type.price, credit_card)
 
+    save
     return response.success?
-  end
-
-  def expiry_date
-    "#{expiry_month}#{expiry_year}"
   end
 
   def create_member
