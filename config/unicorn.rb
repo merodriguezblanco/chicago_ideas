e7ae746a24cef715986c8606c118187342087920
Leandro Marcucci <leanucci@gmail.com>
2013-02-20 16:35:48 -0300

    Zero downtime config for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,6 +21,7 @@ stdout_path "#{@app}/log/unicorn.stdout.log"
 
 # To save some memory and improve performance
 preload_app true
+
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
 
@@ -30,15 +31,50 @@ before_exec do |_|
   ENV["BUNDLE_GEMFILE"] = File.join(@app, 'Gemfile')
 end
 
+# before_fork do |server, worker|
+#   # the following is highly recomended for Rails + "preload_app true"
+#   # as there's no need for the master process to hold a connection
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.connection.disconnect!
+# end
+#
+# after_fork do |server, worker|
+#   # the following is *required* for Rails + "preload_app true",
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.establish_connection
+# end
+
+
+# Zero downtime deploy as per http://goo.gl/5RHvC
+
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{@app}/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server, worker|
   # the following is *required* for Rails + "preload_app true",
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
+  end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
 end
