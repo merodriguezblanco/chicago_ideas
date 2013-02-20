ee5c64f89629baf4b0339b9235bdea4bb27754d4
Leandro Marcucci <leanucci@gmail.com>
2013-02-20 02:42:19 -0300

    Wrapping up new deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,97 +1,50 @@
-# Automatically precompile assets
-load "deploy/assets"
-
-# Execute "bundle install" after deploy, but only when really needed
-require "bundler/capistrano"
-
-# RVM integration
-require "rvm/capistrano"
-
-# Application name
-set :application, "chicago_ideas"
-
-# Application environment
-set :rails_env, :production
-
-# Deploy username and sudo username
-set :user, 'ubuntu'
-set :user_rails, 'ubuntu'
-
-# App Domain
-set :domain, "23.23.114.156"
-
-# We don't want to use sudo (root) - for security reasons
-set :use_sudo, false
-
-# Target ruby version
-set :rvm_ruby_string, '1.9.2'
-
-# System-wide RVM installation
-set :rvm_type, :system
-
-# We use sudo (root) for system-wide RVM installation
-set :rvm_install_with_sudo, true
-
-# git is our SCM
-set :scm, :git
-
-# Use github repository
-set :repository, "git@github.com:frangucc/chicago_ideas.git"
-
-# master is our default git branch
-set :branch, "staging"
-
-# Deploy via github
-set :deploy_via, :remote_cache
-set :deploy_to, "/var/rails/#{application}"
-set :repository_cache, "cached_copy"
-
-# We have all components of the app on the same server
-server domain, :app, :web, :db, :primary => true
 default_run_options[:pty] = true
 
-# Install RVM and Ruby before deploy
-# before "deploy:setup", "rvm:install_rvm"
-# before "deploy:setup", "rvm:install_ruby"
+load 'deploy/assets'
+require 'bundler/capistrano'
+require 'rvm/capistrano'
 
-# Apply default RVM version for the current account
-after "deploy:setup", "deploy:set_rvm_version"
+set :application,             'chicago_ideas'
+set :rails_env,               'production'
+set :user,                    'ciw_deploy'       # 'deployer' is a far too common name
+set :domain,                  '23.23.114.156'
+set :rvm_ruby_string,         '1.9.3-p385'
+set :use_sudo,                false
+set :scm,                     :git
+set :repository,              'git@github.com:frangucc/chicago_ideas.git'
+set :branch,                  'staging'
 
-# Fix log/ and pids/ permissions
-after "deploy:setup", "deploy:fix_setup_permissions"
+set :deploy_via,              :remote_cache
+set :deploy_to,               "/home/#{user}/#{application}"
+set :repository_cache,        'cached_copy'
 
-# Fix permissions
-# before "deploy:start", "deploy:fix_permissions"
-# after "deploy:restart", "deploy:fix_permissions"
-# after "assets:precompile", "deploy:fix_permissions"
-after "deploy", "deploy:symlink_database_and_system_folder"
-after "deploy", "deploy:migrate"
-after "deploy", "deploy:restart"
-# Clean-up old releases
-after "deploy:restart", "deploy:cleanup"
+set :unicorn_config,          "#{current_path}/config/unicorn.rb"
+set :unicorn_pid,             "#{current_path}/tmp/pids/unicorn.pid"
+set :unicorn_binary,          "bash -c 'source /etc/profile.d/rvm.sh && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D'"
+# set :unicorn_binary,          "unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
 
-# Unicorn config
-set :unicorn_config, "#{current_path}/config/unicorn.rb"
-set :unicorn_binary, "bash -c 'source /etc/profile.d/rvm.sh && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D'"
-set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
-set :su_rails, "sudo -u #{user_rails}"
+server domain, :app, :web, :db, :primary => true
 
 namespace :deploy do
+  desc "Copies shared config files"
+  task :copy_shared_files do
+    run "cp #{deploy_to}/shared/config/*.yml #{current_release}/config/"
+  end
+
   task :start, :roles => :app, :except => { :no_release => true } do
-    # Start unicorn server using sudo (rails)
-    run "cd #{current_path} && #{su_rails} #{unicorn_binary}"
+    run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill `cat #{unicorn_pid}`; fi"
+    run "kill `cat #{unicorn_pid}`"
   end
 
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill -s QUIT `cat #{unicorn_pid}`; fi"
+    run "kill -s QUIT `cat #{unicorn_pid}`"
   end
 
   task :reload, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill -s USR2 `cat #{unicorn_pid}`; fi"
+    run "kill -s USR2 `cat #{unicorn_pid}`"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
@@ -99,39 +52,6 @@ namespace :deploy do
     start
   end
 
-  task :set_rvm_version, :roles => :app, :except => { :no_release => true } do
-    run "source /etc/profile.d/rvm.sh && rvm use #{rvm_ruby_string} --default"
-  end
-
-  task :fix_setup_permissions, :roles => :app, :except => { :no_release => true } do
-    run "#{sudo} chgrp #{user_rails} #{shared_path}/log"
-    run "#{sudo} chgrp #{user_rails} #{shared_path}/pids"
-  end
-
-  task :fix_permissions, :roles => :app, :except => { :no_release => true } do
-    # To prevent access errors while moving/deleting
-    run "#{sudo} chmod 775 #{current_path}/log"
-    run "#{sudo} find #{current_path}/log/ -type f -exec chmod 664 {} \\;"
-    run "#{sudo} find #{current_path}/log/ -exec chown #{user}:#{user_rails} {} \\;"
-    run "#{sudo} find #{current_path}/tmp/ -type f -exec chmod 664 {} \\;"
-    run "#{sudo} find #{current_path}/tmp/ -type d -exec chmod 775 {} \\;"
-    run "#{sudo} find #{current_path}/tmp/ -exec chown #{user}:#{user_rails} {} \\;"
-    run "#{sudo} find #{current_path}/config/ -type f -exec chmod 664 {} \\;"
-    run "#{sudo} find #{current_path}/config/ -type d -exec chmod 775 {} \\;"
-    run "#{sudo} find #{current_path}/config/ -exec chown #{user}:#{user_rails} {} \\;"
-  end
-
-  task :symlink_database_and_system_folder do
-    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/mongoid.yml #{current_path}/config/mongoid.yml"
-    run "ln -nfs #{shared_path}/config/amazon_s3.yml #{current_path}/config/amazon_s3.yml"
-  end
-
-  task :invoke_rake do
-    run("cd #{deploy_to}/current && bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}")
-  end
-
-  # Precompile assets only when needed
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       # If this is our first deploy - don't check for the previous version
@@ -149,16 +69,9 @@ namespace :deploy do
   end
 end
 
-# Helper function
 def remote_file_exists?(full_path)
   'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
 end
 
-def run_remote_rake(rake_cmd)
-  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+after 'deploy:update_code', 'deploy:copy_shared_files'
 
-  cmd = "cd #{latest_release} && bundle exec rake RAILS_ENV=#{rails_env} #{rake_cmd}"
-  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
-  run cmd
-  set :rakefile, nil if exists?(:rakefile)
-end
